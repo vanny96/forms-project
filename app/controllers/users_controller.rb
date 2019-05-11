453708d646041b8aa3f4050689b015015070f169
Giovanni Barbaro <giovannibarbaro7@gmail.com>
2019-05-11 14:06:04 +0200

    Completed the 'Railsy-er Forms with #form_for' section

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  include UsersHelper
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+      puts @user.errors.messages
+    end
+  end
+end
